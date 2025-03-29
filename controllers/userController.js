3b1712ce3c8df0389bd42a4ed48e7b866e30808d
kyky <kyle.vuanh@gmail.com>
2025-03-29 15:16:15 +0700

    [CREATE] getAllExcel

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const excel = require('exceljs');
 
 module.exports = userController = {
     getAll: async(req, res) => {
@@ -11,7 +12,23 @@ module.exports = userController = {
     },
     getAllExcel: async(req, res) => {
         try {
-            
+            const users = await User.find();
+            const workbook = new excel.Workbook();
+            const worksheet = workbook.addWorksheet('AR');
+            worksheet.columns = [
+                { header: 'ordinal_number', key: 'ordinal_number', width: 15 },
+                { header: 'full_name', key: 'full_name', width: 25 },
+                { header: 'gender', key: 'gender', width: 10 },
+                { header: 'area', key: 'area', width: 10 },
+                { header: 'isPresent', key: 'isPresent', width: 10}
+            ];
+            users.forEach(user => {
+                worksheet.addRow(user.toObject());
+            });
+            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            res.setHeader('Content-Disposition', 'attachment; filename=AR.xlsx');
+            await workbook.xlsx.write(res);
+            res.end();
         } catch (err) {
             res.status(500).json(err);
         }
