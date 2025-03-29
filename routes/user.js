4af4c89d4980242ca2f9103b6999b62dc2d80c0f
kyky <kyle.vuanh@gmail.com>
2025-03-29 14:53:26 +0700

    [UPDATE] User Route Typo

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const userController = require('../controllers/userController');
 const router = require('express').Router();
 
 router.get('/user', userController.getAll);
-router.get('/user/exel', userController.getAllExel);
+router.get('/user/excel', userController.getAllExel);
 router.put('/user', userController.updateAll);
 
 module.exports = router;
