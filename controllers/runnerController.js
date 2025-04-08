fb1007933e18b8e131d50f4365d7151912dacf21
kyky <kyle.vuanh@gmail.com>
2025-04-08 22:10:50 +0700

    [CREATE] Runner getAll

--- controllers/runnerController.js
+++ controllers/runnerController.js
@@ -1,7 +1,10 @@
+const Runner = require('../models/Runner');
+
 module.exports = runnerController = {
     getAll: async(req, res) => {
         try {
-
+            const runners = await Runner.find().select('-_id');
+            res.status(200).json(runners);
         } catch (err) {
             res.status(500).json(err);
         }
