1692710020630de5e358fe6f8ee2fdfd49646dc5
kyky <kyle.vuanh@gmail.com>
2025-04-08 19:38:12 +0700

    [CREATE] Project Init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(8000, () => {
+    console.log('Server is running.');
+});
