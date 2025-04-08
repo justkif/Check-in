9edc3cc328bb3d511558fce4d546105ed045113f
kyky <kyle.vuanh@gmail.com>
2025-04-08 19:40:22 +0700

    [CREATE] MongoDB Connection

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 require('dotenv').config();
+
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Database Connected.');
+    } catch (err) {
+        console.log(err);
+    }
+})();
 
 app.use(cors());
 app.use(express.json());
