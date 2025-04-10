f7762e5c1d722d79c536260d7683a08923b3391c
kyky <kyle.vuanh@gmail.com>
2025-04-10 08:56:11 +0700

    [CREATE] Runner generateQR

--- controllers/runnerController.js
+++ controllers/runnerController.js
@@ -1,4 +1,5 @@
 const Runner = require('../models/Runner');
+const qrCode = require('qrcode');
 
 module.exports = runnerController = {
     getAll: async(req, res) => {
@@ -22,7 +23,16 @@ module.exports = runnerController = {
     },
     generateQR: async(req, res) => {
         try {
-
+            const runner = await Runner.findOne({ ordinalNumber: req.params.username });
+            if (!runner) {
+                return res.status(404).json('Runner not found.');
+            }
+            qrCode.toDataURL(runner._id.toString(), function(err, url) {
+                if (err) {
+                    return res.status(500).json(err);
+                }
+                res.status(200).send(`<img src="${url}">`);
+            });
         } catch (err) {
             res.status(500).json(err);
         }
--- routes/runner.js
+++ routes/runner.js
@@ -4,7 +4,7 @@ const jwtController = require('../controllers/jwtController');
 
 router.get('/runner', jwtController.verifyManager, runnerController.getAll);
 router.get('/runner/:runnerId', jwtController.verifyScanner, runnerController.getOne);
-router.get('/runner/generateQR/:runnerId', jwtController.verifyRunner, runnerController.generateQR);
+router.get('/runner/generateQR/:username', jwtController.verifyRunner, runnerController.generateQR);
 router.get('runner/exportExcel', jwtController.verifyManager, runnerController.exportExcel);
 router.post('/runner/scanQR', jwtController.verifyScanner, runnerController.scanQR);
 router.put('/runner/reset', jwtController.verifyManager, runnerController.reset);
